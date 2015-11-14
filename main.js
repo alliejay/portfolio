a764365abed6499d82a7c3a88c5c11cbce8d417d
Alison Arsenault <AlisonHorta@Alisons-MacBook-Pro.local>
2015-11-14 16:58:04 -0500

    Initial commit - basic site complete

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  page.init();
+});
+
+var page = {
+
+  init: function () {
+    page.initStyling();
+    page.initEvents();
+  },
+
+  initStyling: function () {
+
+  },
+
+  initEvents: function () {
+    page.navLinks();
+    page.spanLinks();
+  },
+
+  navLinks: function() {
+    $('nav').on('click', 'li', function(event) {
+      event.preventDefault();
+      var selectedSection = "." + $(this).attr('rel');
+      $(selectedSection).removeClass('hidden');
+      $(selectedSection).siblings('article').addClass('hidden');
+    })
+  },
+
+  spanLinks: function() {
+    $('.content').on('click', 'span', function (event) {
+      event.preventDefault();
+      var selectedSpanSection = "." + $(this).attr('rel');
+      console.log(selectedSpanSection);
+      $(selectedSpanSection).removeClass('hidden');
+      $(selectedSpanSection).siblings('article').addClass('hidden');
+    })
+  }
+
+};
